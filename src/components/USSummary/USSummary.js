16183d6017c75ac94e80a92ab3d7ccbccb8e9a82
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-10-15 17:16:12 -0500

    Add commas to numbers on US Summary

--- src/components/USSummary/USSummary.js
+++ src/components/USSummary/USSummary.js
@@ -1,33 +1,68 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import NumberFormat from 'react-number-format';
 
-function USSummary () {
-    const [usSummary, setUsSummary] = useState ([]);
-    const getApiData = async () => {
-        const url = "https://covid-api.mmediagroup.fr/v1/cases";
-        try {
-            const response = await fetch (url);
-            const data = await response.json();
-            console.log(data.US.All);
-            setUsSummary(data.US.All);
-        } catch (error) {
-            console.log(error);
-        }
+function USSummary() {
+  const [usSummary, setUsSummary] = useState([]);
+  const getApiData = async () => {
+    const url = 'https://covid-api.mmediagroup.fr/v1/cases';
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.US.All);
+      setUsSummary(data.US.All);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect (() => {
-        getApiData ();
-    }, []);
-    return (
-        <div className = "usSummaryData">
-            <h2>US Summary Data</h2>
-            <div className = "data">
-                <li>Confirmed: {usSummary.confirmed.toLocaleString("en-US")}</li>
-                <li>Recovered: {usSummary.recovered.toLocaleString("en-US")}</li>
-                <li>Deaths: {usSummary.deaths.toLocaleString("en-US")}</li>
-            </div>
-        </div>
-
-
-    );
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
+  return (
+    <div className="usSummaryData">
+      <h2>US Summary Data</h2>
+      <div className="data">
+        <li>
+          Confirmed:{' '}
+          <NumberFormat
+            thousandsGroupStyle="thousand"
+            value={usSummary.confirmed}
+            prefix="   "
+            decimalSeparator="."
+            displayType="text"
+            type="text"
+            thousandSeparator={true}
+            allowNegative={true}
+          />
+        </li>
+        <li>
+          Recovered:{' '}
+          <NumberFormat
+            thousandsGroupStyle="thousand"
+            value={usSummary.recovered}
+            prefix="   "
+            decimalSeparator="."
+            displayType="text"
+            type="text"
+            thousandSeparator={true}
+            allowNegative={true}
+          />
+        </li>
+        <li>
+          Deaths:{' '}
+          <NumberFormat
+            thousandsGroupStyle="thousand"
+            value={usSummary.deaths}
+            prefix="   "
+            decimalSeparator="."
+            displayType="text"
+            type="text"
+            thousandSeparator={true}
+            allowNegative={true}
+          />
+        </li>
+      </div>
+    </div>
+  );
 }
 
 export default USSummary;
