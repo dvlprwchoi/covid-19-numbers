8ffef1dd40923bb23666cb90eea852f725b7714b
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-10-15 20:16:37 -0500

    Search by State Texas

--- src/components/SearchByState/SearchByState.js
+++ src/components/SearchByState/SearchByState.js
@@ -8,8 +8,8 @@ function SearchByState() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data.US.All);
-      setSearchByState(data.US.All);
+      console.log(data.US.Texas);
+      setSearchByState(data.US.Texas);
     } catch (error) {
       console.log(error);
     }
@@ -17,9 +17,31 @@ function SearchByState() {
   useEffect(() => {
     getApiData();
   }, []);
+
+  //   function handleSubmit(e) {
+  //     alert('State name was submitted: ' + this.state.value);
+  //     e.preventdefault();
+  //   }
+
+  //   function handleChange(e) {
+  //     this.setState({ value: e.target.value });
+  //   }
+
   return (
     <div className="searchByState">
       <h2>Search By State</h2>
+      {/* <div className="stateName">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            State:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+      </div> */}
       <div className="data">
         <li>
           Confirmed:{' '}
