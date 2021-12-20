d3575ef9bc449d52b91c01ef603439a8e8965857
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-20 12:53:39 -0600

    Add input box and search button

--- src/components/SearchByState/SearchForm/SearchForm.js
+++ src/components/SearchByState/SearchForm/SearchForm.js
@@ -1,5 +1,25 @@
 function SearchForm() {
-  return <div className="searchForm">This is search Form</div>;
+  return (
+    <div className="searchForm">
+      <form className="form">
+        <label className="stateSearchInputLabel" htmlFor="stateName">
+          <input
+            type="text"
+            className="stateSearchInputBox"
+            placeholder="Type your state"
+            name="stateName"
+            id=";stateName"
+            autoFocus
+          ></input>
+        </label>
+        <input
+          type="submit"
+          className="searchInputSubmitButton"
+          value="Search"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default SearchForm;
