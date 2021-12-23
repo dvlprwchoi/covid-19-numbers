2b38dc41c8f7393eba61b4e03b56593d1053ea1e
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-23 10:25:56 -0600

    Working but need error handling for wrong name to correct name

--- src/components/SearchByCountry/SearchData/SearchData.js
+++ src/components/SearchByCountry/SearchData/SearchData.js
@@ -1,12 +1,23 @@
 import NumberFormat from 'react-number-format';
 
-function SearchData({ searchByCountry }) {
+function SearchData({ searchByCountry, countryName, countryNames }) {
   // console.log(searchByCountry);
   if (searchByCountry.length === 0) {
     return (
       <div className="searchData">
         <div className="no-data">
           <li>No data to show</li>
+        </div>
+      </div>
+    );
+  } else if (countryNames.includes(countryName) === false) {
+    return (
+      <div className="searchData">
+        <div className="no-data">
+          <li>
+            We CANNOT find your country name on the country list OR DOES NOT
+            matches...Try it again!
+          </li>
         </div>
       </div>
     );
